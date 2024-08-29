1c3b0330696d572edebd72ce857e9e1363ee43d1
agrimm17 <alexandergrimm17@gmail.com>
2024-08-28 17:29:42 -0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,16 @@
 
+console.log('This is a popup!');
+
+
+let simple = (i) => {
+    if (i > 3) {
+        return "Good Job!"
+    }
+    alert("Hello?")
+    i++
+    setTimeout(() => {
+        simple(i)
+    }, 6000)
+}
+
+simple(0)
